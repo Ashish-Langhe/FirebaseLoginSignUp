48da81fe72b9abd2ae93f5b8a83afc2329419b36
Ashish-Langhe <langheashish389@gmail.com>
2024-10-21 22:39:45 +0530

    Initial Commit

---
+++ FirebaseAuth/FirebaseAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthApp.swift
+//  FirebaseAuth
+//
+//  Created by Ashish Langhe on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
